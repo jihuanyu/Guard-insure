b90799fea6fc557d1ff4b4117531c0604a1bf093
Jihuanyu12138 <ji2240224966@163.com>
2020-12-29 18:58:42 +0800

    'guardhome'

---
+++ components/common/footer.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="footer-container">
+    <div class="footer_bottom">
+      <p>Powered by payaso.io core team</p>
+      <div class="footer_top">
+        <div>
+          <a href="">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-Twitter"></use>
+            </svg>
+          </a>
+          <a href="">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-Telegram"></use>
+            </svg>
+          </a>
+          <a href="">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-Discord"></use>
+            </svg>
+          </a>
+          <a href="">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-medium"></use>
+            </svg>
+          </a>
+        </div>
+      </div>
+      <Langauage class="langauage" />
+    </div>
+  </div>
+</template>
+<script>
+import Langauage from "~/components/common/langauage.vue";
+import "~/assets/svg/iconfont.js";
+export default {
+  props: ["padding"],
+  name: "p-footer",
+  components: { Langauage },
+};
+</script>
+<style lang="scss" scoped>
+@import "~/assets/css/base.scss";
+@media screen and (min-width: 750px) {
+  .footer-container {
+    width: 100%;
+    height: 80px;
+    background: #fff;
+    display: flex;
+    min-width: 1200px;
+    box-sizing: border-box;
+    .footer_top {
+      flex: 1;
+      display: flex;
+      justify-content: flex-end;
+      div {
+        width: 216px;
+        display: flex;
+        justify-content: space-between;
+        margin-right: 80px;
+        a {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          img {
+            width: 24px;
+            height: 24px;
+          }
+          span {
+            margin-top: 4px;
+            font-size: 14px;
+            color: #787878;
+          }
+        }
+      }
+    }
+    .footer_bottom {
+      padding: 0 6.25vw;
+      width: 100%;
+      height: 80px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      display: flex;
+      background: #f5f7fb;
+      p {
+        font-size: 14px;
+        font-weight: 400;
+        color: #919aa6;
+      }
+    }
+  }
+}
+@media screen and (min-width: 750px) and (max-width: 1280px) {
+  .footer_bottom {
+    padding: 0 50px !important;
+  }
+}
+
+@media screen and (max-width: 750px) {
+  .footer-container {
+    width: 100%;
+    height: 176px;
+    display: flex;
+    flex-direction: column;
+    background: #f5f7fb;
+    .footer_top {
+      width: 100%;
+      margin-top: 40px;
+      div {
+        display: flex;
+        justify-content: space-between;
+        width: 200px;
+        a {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          img {
+            width: 24px;
+            height: 24px;
+          }
+          span {
+            margin-top: 4px;
+            font-size: 14px;
+            color: #787878;
+          }
+        }
+      }
+    }
+    .footer_bottom {
+      margin-top: 30px;
+      width: 100%;
+      height: 176px;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      padding: 0 16px !important;
+      display: flex;
+      p {
+        width: 100%;
+        font-size: 14px;
+        font-weight: 400;
+        color: #919aa6;
+      }
+      .langauage {
+        display: none;
+      }
+    }
+  }
+}
+.icon {
+  width: 24px;
+  height: 24px;
+  vertical-align: -0.15em;
+  fill: #787878;
+  overflow: hidden;
+}
+.footer_top {
+  a:hover {
+    .icon {
+      fill: #4364e8;
+    }
+    span {
+      color: #4364e8 !important;
+    }
+  }
+}
+</style>
---
+++ components/insurance/title.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="trade_title">
+    <div class="text">
+      <h3>{{ $t("Banner.Title") }}</h3>
+      <span>{{ $t("Banner.Text") }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.trade_title {
+  height: 220px;
+  background: #ffffff;
+  margin-bottom: 15px;
+  padding: 0 60px;
+  display: flex;
+  justify-content: space-between;
+  background-image: url("../../assets/img/helmet/banner_ill.png");
+  background-size: 344px 200px;
+  background-position: 95% 20px;
+  background-repeat: no-repeat;
+  .text {
+    h3 {
+      width: 373px;
+      font-size: 24px;
+      font-weight: 600;
+      color: #121212;
+      line-height: 33px;
+      margin: 64px 0 16px 0;
+    }
+    span {
+      color: #919aa6;
+      line-height: 20px;
+    }
+  }
+}
+</style>
